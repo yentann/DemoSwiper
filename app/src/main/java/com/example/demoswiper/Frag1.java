20280ac0a9bab3ee2a22d5179596045eff3fe316
TAN QIAO YUE <17045679@rp.edu.sg>
2019-07-18 16:04:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demoswiper/Frag1.java
@@ -0,0 +1,25 @@
+package com.example.demoswiper;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Frag1 extends Fragment {
+
+
+    public Frag1() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag1, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/demoswiper/Frag2.java
@@ -0,0 +1,26 @@
+package com.example.demoswiper;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Frag2 extends Fragment {
+
+
+    public Frag2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag2, container, false);
+    }
+
+}
